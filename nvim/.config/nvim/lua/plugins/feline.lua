a3ea8afd64b3c3a73a5a0ced6672baa012a9adfd
Shawn Petros <shawn.petros@gmail.com>
2025-03-10 16:29:13 -0700

    chore(nvim): swap lua line for feline

---
+++ nvim/.config/nvim/lua/plugins/feline.lua
@@ -0,0 +1,9 @@
+return {
+	"feline-nvim/feline.nvim",
+	after = "catppuccin",
+	config = function()
+		require("feline").setup {
+			components = require("catppuccin.groups.integrations.feline").get(),
+		}
+	end
+}
