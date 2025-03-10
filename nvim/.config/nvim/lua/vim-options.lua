4b91309d8b6f4686ec411fa7422bb78b2771ad0d
Shawn Petros <shawn.petros@gmail.com>
2025-03-10 15:14:22 -0700

    feat(tmux/vim): tmux and vim pane navigation

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -13,5 +13,16 @@ vim.g.maplocalleader = "\\"
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { desc = "Turn off search hl" })
 
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', { desc = "Switch to window left", noremap = true, silent = true })
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', { desc = "Switch to window down", noremap = true, silent = true })
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', { desc = "Switch to window up", noremap = true, silent = true })
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', { desc = "Switch to window right", noremap = true, silent = true })
+
+vim.keymap.set('n', '<C-->', ':vsplit<CR>', { desc = "Switch to window up", noremap = true, silent = true })
+vim.keymap.set('n', '<C-\\>', ':split k<CR>', { desc = "Switch to window up", noremap = true, silent = true })
+
+vim.keymap.set('n', '<M-h>', '0', { desc = "Beginning of line", noremap = true, silent = true })
+vim.keymap.set('n', '<M-l>', '$', { desc = "End of line", noremap = true, silent = true })
+
 -- some qol upgrades
 require("autocmds")
